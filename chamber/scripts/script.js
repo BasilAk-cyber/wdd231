259569d1d5f9d53d5d34a018ac10211ba9b6c900
BasilAk-cyber <aubasil07@gmail.com>
2026-01-25 21:00:00 +0100

    commit

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -4,11 +4,18 @@ const lastModified = document.querySelector('#lastModified');
 const navMenu = document.querySelector('.main-nav');
 const navLinks = document.querySelectorAll('.nav-links');
 const hamburger = document.querySelector('.hamburger');
+const directoryDisplay = document.querySelector('.business-card-section');
+const viewGridBtn = document.querySelector('.view-grid');
 
 
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
+})
+
+viewGridBtn.addEventListener("click", () => {
+    directoryDisplay.classList.toggle("active");
+    viewGridBtn.textContent = 'Display Grid';
 })
 
 navLinks.forEach(element => {
