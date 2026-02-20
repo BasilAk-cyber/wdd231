f949e86af7102136a6b783cbff662c0b2b5a2e31
BasilAk-cyber <142195641+BasilAk-cyber@users.noreply.github.com>
2026-02-20 20:08:18 +0100

    Update script.js

--- movieapi/scripts/script.js
+++ movieapi/scripts/script.js
@@ -33,7 +33,7 @@ function saveFavorites() {
   localStorage.setItem("favMovies", JSON.stringify(favorites));
   //document.getElementById("fav-count").textContent = favorites.length;
 }
-x
+
 function isFavorite(id) {
   return favorites.some(m => m.id === id);
 }
